0a4b7f83b227185c50b45797db0548ed84b4fe1e
Andreas Lausen <lausen.andreas@gmail.com>
2026-04-09 17:30:57 +0200

    fixex acceptance tests

--- src/backend/Talk/ConferenceExample.Talk.AcceptanceTests/SetupTestDependencies.cs
+++ src/backend/Talk/ConferenceExample.Talk.AcceptanceTests/SetupTestDependencies.cs
@@ -1,5 +1,6 @@
 using ConferenceExample.EventStore;
 using ConferenceExample.Talk.Application;
+using ConferenceExample.Talk.Application.Commands;
 using ConferenceExample.Talk.Domain.Repositories;
 using ConferenceExample.Talk.Persistence;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +17,7 @@ public class SetupTestDependencies
         services.AddSingleton<IEventStore, InMemoryEventStore>();
         services.AddSingleton<IEventBus, InMemoryEventBus>();
         services.AddScoped<ITalkRepository, TalkRepository>();
+        services.AddScoped<ISubmitTalkCommandHandler, SubmitTalkCommandHandler>();
         services.AddScoped<ITalkService, TalkService>();
         return services;
     }
